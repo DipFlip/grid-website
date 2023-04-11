ddaefb5aeea184948565e9d30e294c69067968ec
Emil Rofors <emirof@gmail.com>
2023-04-10 22:03:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# grid-website
+ A CSS javascript website background that reacts to the cursor
---
+++ script.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById('gridCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const gridSpacing = 39; // Approximately 1 cm in pixels
+
+function drawGrid(mouseX, mouseY) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = '#808080';
+    ctx.lineWidth = 1;
+
+    for (let x = 0; x < canvas.width; x += gridSpacing) {
+        const distanceX = (x - mouseX);
+        const offsetX = calculateOffset(distanceX);
+        ctx.beginPath();
+        ctx.moveTo(x + offsetX, 0);
+        ctx.lineTo(x + offsetX, canvas.height);
+        ctx.stroke();
+    }
+
+    for (let y = 0; y < canvas.height; y += gridSpacing) {
+        const distanceY = (y - mouseY);
+        const offsetY = calculateOffset(distanceY);
+        ctx.beginPath();
+        ctx.moveTo(0, y + offsetY);
+        ctx.lineTo(canvas.width, y + offsetY);
+        ctx.stroke();
+    }
+}
+
+function calculateOffset(distance) {
+    let normalizedDistance = Math.abs(distance / gridSpacing);
+    // normalizedDistance = normalizedDistance + 0.177;
+    if (normalizedDistance >= 20 || normalizedDistance <= 0.0001) {
+        return 0;
+    } else {
+        console.log("distance: " + distance);
+        console.log("normalizedDistance: " + normalizedDistance);
+        let difference = -normalizedDistance + 1.3;
+        // let offsetX = (3.5 - ((4.0 - normalizedDistance) ** (-1) * (normalizedDistance ** (-1.5)))) * Math.exp(-normalizedDistance+3)/100;
+        let offsetX = 2.718**((-1*difference**2)) / 10;
+        // offsetX = offsetX < 0.0001 ? 0 : offsetX * gridSpacing;
+        console.log("offsetX: " + offsetX);
+        if (distance < 0) {
+            offsetX = -offsetX;
+        }
+        return offsetX * gridSpacing;
+    }
+}
+
+canvas.addEventListener('mousemove', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    drawGrid(mouseX, mouseY);
+});
+
+// Initial draw
+drawGrid(-1, -1); // Pass -1, -1 to initialize grid without any mouse interaction
